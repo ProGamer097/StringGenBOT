84aa626c7a725cbdb15e228797221306788749d1
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-01 21:01:47 +0530

    Update users.py

--- AMSSGEN/db/users.py
+++ AMSSGEN/db/users.py
@@ -3,12 +3,11 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.users
-
+db = mongo.chats
+db = db.chatsdb
 db = db.users
 
-
-
-
+#users
 async def get_users():
   user_list = []
   async for user in db.users.find({"user": {"$gt": 0}}):
@@ -37,3 +36,32 @@ async def del_user(user):
     return
   else:
     await db.users.delete_one({"user": user})
+
+#chats
+
+async def get_chats():
+  chat_list = []
+  async for chat in db.chats.find({"chat": {"$lt": 0}}):
+    chat_list.append(chat['chat'])
+  return chat_list
+
+async def get_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
+    return True
+  else:
+    return False
+
+async def add_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
+    return
+  else:
+    await db.chats.insert_one({"chat": chat})
+
+async def del_chat(chat):
+  chats = await get_chats()
+  if not chat in chats:
+    return
+  else:
+    await db.chats.delete_one({"chat": chat})
