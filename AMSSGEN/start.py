26ec0c66e0fa8e41bdb67c4c6b6f5a9b1d081ae5
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-01 21:17:31 +0530

    Update start.py

--- AMSSGEN/start.py
+++ AMSSGEN/start.py
@@ -1,8 +1,28 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
+from pyrogram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+import random
 from config import OWNER_ID
 
+AM_PIC = [
+    "https://telegra.ph/file/7c25ef427c9f3cded5577.jpg",
+    "https://telegra.ph/file/625d235cc0a22fb8525b5.jpg",
+    "https://telegra.ph/file/1c62254d59baf7f968ba7.jpg",
+    "https://telegra.ph/file/7a0553bd4664486ab3008.jpg",
+    "https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg",
+    "https://telegra.ph/file/2773dec98d87b8562618c.jpg",
+    "https://telegra.ph/file/80353d02e0368b71d2666.jpg",
+    "https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg",
+    "https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg",
+    "https://telegra.ph/file/8371bcd8952d089f9ec05.jpg",
+    "https://telegra.ph/file/f970e559dd1bb96fced1a.jpg",
+    "https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"
+]
 
 def filter(cmd: str):
     return filters.private & filters.incoming & filters.command(cmd)
@@ -10,7 +30,8 @@ def filter(cmd: str):
 @Client.on_message(filter("start"))
 async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
-    await bot.send_message(
+    await bot.reply_photo(
+         photo=random.choice(AM_PIC),
         chat_id=msg.chat.id,
         text=f"""𝘏𝘦𝘺 {msg.from_user.mention}🍷,
 
