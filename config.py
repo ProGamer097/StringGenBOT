12207dab3d96aea01a6ad2395a4aa7f1428da6d6
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-02-06 04:07:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID","12227067"))
 API_HASH = getenv("API_HASH","b463bedd791aa733ae2297e6520302fe")
-BOT_TOKEN = getenv("BOT_TOKEN","6615530566:AAEy_Pc8C62_AqZLMZ8GnSYFY7jaeqLN9eM")
+BOT_TOKEN = getenv("BOT_TOKEN","6615530566:AAE4eTgWaeLomDvGZTETK4dOnZ1rxx7eb60")
 OWNER_ID = int(getenv("OWNER_ID", "5360305806"))
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://AMBOT:AMBOT@ambot.uecutzy.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", "AMBOTYT")
