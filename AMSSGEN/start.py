36f67b86e65dd978e514226ae94058649533018b
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-08 23:16:19 +0530

    Update start.py

--- AMSSGEN/start.py
+++ AMSSGEN/start.py
@@ -10,18 +10,18 @@ import random
 from config import OWNER_ID
 
 AM_PIC = [
-    "https://telegra.ph/file/7c25ef427c9f3cded5577.jpg",
-    "https://telegra.ph/file/625d235cc0a22fb8525b5.jpg",
-    "https://telegra.ph/file/1c62254d59baf7f968ba7.jpg",
-    "https://telegra.ph/file/7a0553bd4664486ab3008.jpg",
-    "https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg",
-    "https://telegra.ph/file/2773dec98d87b8562618c.jpg",
-    "https://telegra.ph/file/80353d02e0368b71d2666.jpg",
-    "https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg",
-    "https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg",
-    "https://telegra.ph/file/8371bcd8952d089f9ec05.jpg",
-    "https://telegra.ph/file/f970e559dd1bb96fced1a.jpg",
-    "https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg",
+    "https://graph.org/file/4681f0896068e3b5e5054.jpg"
 ]
 
 def filter(cmd: str):
@@ -32,7 +32,7 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_photo(
          chat_id=msg.chat.id,
-         photo=random.choice(AM_PIC),
+         photo=random.choice(Naruto_PIC),
          caption=f"""𝘏𝘦𝘺 {msg.from_user.mention}🍷,
 
 𝘐 𝘈𝘮 {me2},
@@ -41,15 +41,15 @@ async def start(bot: Client, msg: Message):
 𝘍𝘜𝘓𝘓𝘠 𝘚𝘈𝘍𝘌 𝘈𝘕𝘋 𝘚𝘌𝘊𝘜𝘙𝘌 
 𝘕𝘖 𝘌𝘙𝘙𝘖𝘙
 
-𝙈𝙖𝙙𝙚 𝘽𝙮 : [AMBOT](tg://user?id={OWNER_ID}) !""",
+𝙈𝙖𝙙𝙚 𝘽𝙮 : [Naruto](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="𝐺𝐸𝑁𝐸𝑅𝐴𝑇𝐸 𝑆𝑇𝑅𝐼𝑁𝐺", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url="https://t.me/AM_YTSUPPORT"),
-                    InlineKeyboardButton("Uᴘᴅᴀᴛᴇs", url="https://t.me/AMBOTYT")
+                    InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url="https://t.me/naruto_support1"),
+                    InlineKeyboardButton("Uᴘᴅᴀᴛᴇs", url="https://t.me/naruto_support1")
                 ]
             ]
         ),
