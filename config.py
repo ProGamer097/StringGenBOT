5b26e5fdc0426cf083d30e779bfdbf3e7eb38b71
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-08 23:19:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","12227067"))
-API_HASH = getenv("API_HASH","b463bedd791aa733ae2297e6520302fe")
-BOT_TOKEN = getenv("BOT_TOKEN","6615530566:AAEQrjOUH6_TjWI6l9NL113AWZOnzP5Obbc")
-OWNER_ID = int(getenv("OWNER_ID", "5360305806"))
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://AMBOT:AMBOT@ambot.uecutzy.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "AMBOTYT")
+API_ID = int(getenv("API_ID","20457610"))
+API_HASH = getenv("API_HASH","b7de0dfecd19375d3f84dbedaeb92537")
+BOT_TOKEN = getenv("BOT_TOKEN","7198737383:AAFF_m7PgyD6RK8a31JkB-zEHNYXyIms5ek")
+OWNER_ID = int(getenv("OWNER_ID", "6590287973"))
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://vinamratiwari579:m6YDRYH8HbwuEqxt@cluster0.x7ac1wt.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("MUST_JOIN", "naruto_support1")
