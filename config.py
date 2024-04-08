840c5fcb5c2817457d8d5ce8fd2db378749dd6e6
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-08 23:40:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID","20457610"))
 API_HASH = getenv("API_HASH","b7de0dfecd19375d3f84dbedaeb92537")
-BOT_TOKEN = getenv("BOT_TOKEN","6787446732:AAH9n3KoIcICz-amVlvpmo6QywEx1A65AGo")
+BOT_TOKEN = getenv("BOT_TOKEN","6787446732:AAGjXrIb_iDG5aUPr8PSztXatfq-6kshsiQ")
 OWNER_ID = int(getenv("OWNER_ID", "6590287973"))
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://vinamratiwari579:m6YDRYH8HbwuEqxt@cluster0.x7ac1wt.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", "naruto_support1")
